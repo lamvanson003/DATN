86b74351d11d55d3e1be2cb91151a78840454249
PHẠM THẾ TOÀN <thetoan.0112@gmail.com>
2024-09-29 15:48:56 +0700

    update1

--- FE/src/pages/user/index.jsx
+++
@@ -1,4 +0,0 @@
-export { default as Home } from "./Home";
-export { default as Product } from "./Product";
-export { default as ProductDetail } from "./ProductDetail";
-export { default as Cart } from "./Cart";
