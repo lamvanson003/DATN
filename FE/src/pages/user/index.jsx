632f2f0088a2f0c8e7d5c0cda8bd845e4f185923
PVkhang373 <vohoangly609@gmail.com>
2024-09-29 21:35:45 +0700

    add index

--- FE/src/pages/user/index.jsx
+++ FE/src/pages/user/index.jsx
@@ -2,3 +2,14 @@ export { default as Home } from "./Home";
 export { default as Product } from "./Product";
 export { default as ProductDetail } from "./ProductDetail";
 export { default as Cart } from "./Cart";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
