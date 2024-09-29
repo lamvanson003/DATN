c69b5f1bb81746ee34c107ef49f0249f4b938068
VoHieu906 <vohieutd09@gmail.com>
2024-09-29 15:00:08 +0700

    updateFE

---
+++ FE/src/components/Boxpro.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Boxpro = () => {
+  return <div>Boxpro</div>;
+};
+
+export default Boxpro;
---
+++ FE/src/pages/user/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>{/* Nội dung */}</div>;
+};
+
+export default Home;
