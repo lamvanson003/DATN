bfe4be1b6a42664f489e609930ace2f5f033d558
VoHieu906 <vohieutd09@gmail.com>
2024-10-04 22:29:40 +0700

    add favor (1)

--- FE/src/main.jsx
+++ FE/src/main.jsx
@@ -4,13 +4,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { CartProvider } from "./context/Cart.jsx";
+import { FavorProvider } from "./context/Favor.jsx";
 import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <CartProvider>
-      <App />
+      <FavorProvider>
+        <App />
+      </FavorProvider>
     </CartProvider>
   </BrowserRouter>
 );
--- FE/src/pages/user/Public.jsx
+++ FE/src/pages/user/Public.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Header, Footer } from "../../components";
 import { Outlet, useLocation } from "react-router-dom";
 import { CartContext } from "../../context/Cart";
+import { FavorContext } from "../../context/Favor";
 const Public = () => {
   const location = useLocation();
   const hideHeaderFooterRoutes = ["/login", "/signup"];
@@ -9,11 +10,16 @@ const Public = () => {
     location.pathname
   );
   const { cartItems } = useContext(CartContext);
-  console.log(cartItems.length);
+  const { favorItems } = useContext(FavorContext);
 
   return (
     <div>
-      {!shouldHideHeaderFooter && <Header cartItemAmout={cartItems.length} />}
+      {!shouldHideHeaderFooter && (
+        <Header
+          cartItemAmout={cartItems.length}
+          favorItemAmount={favorItems.length}
+        />
+      )}
       <div>
         <Outlet />
       </div>
