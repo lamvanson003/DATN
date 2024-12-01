1e6fd6b2e2d50fc639b8448614be3bc2208d3313
VoHieu906 <vohieutd09@gmail.com>
2024-12-01 20:21:01 +0700

    add feature post

--- FE/src/apis/post.jsx
+++ FE/src/apis/post.jsx
@@ -17,4 +17,14 @@ export const postApi = {
       console.log("lỗi khi fetch dữ liệu: ", err);
     }
   },
+  getFeature: async () => {
+    try {
+      const res = await axios.get(
+        "http://127.0.0.1:8000/api/posts/is_featured"
+      );
+      return res.data.data;
+    } catch (err) {
+      console.log("lỗi khi fetch dữ liệu: ", err);
+    }
+  },
 };
