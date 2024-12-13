a24bf91e2799ac44a223f7fa0d6ea80a380eab80
khoanha12122004 <135188796+khoanha12122004@users.noreply.github.com>
2024-12-13 20:54:26 +0700

    update discount

--- BE/resources/views/discount/scripts/script.blade.php
+++ BE/resources/views/discount/scripts/script.blade.php
@@ -3,13 +3,17 @@
     $('#discount_type').on('change', function() {
         var discountType = $(this).val();
 
-        if (discountType == 2) {
+        if (discountType == 2) { 
             $('#discount_money').removeClass('d-none'); 
+            $('#discount_money input').prop('disabled', false); 
             $('#discount_percent').addClass('d-none'); 
+            $('#discount_percent input').prop('disabled', true); 
             $('#percent_value').val('');
-        } else {
+        }  else { 
             $('#discount_money').addClass('d-none'); 
+            $('#discount_money input').prop('disabled', true); 
             $('#discount_percent').removeClass('d-none');
+            $('#discount_percent input').prop('disabled', false); 
             $('#discount_value').val('');
         }
     });
