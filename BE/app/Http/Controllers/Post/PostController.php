d22b5ce6238aed84076d0574f38051ce640ed422
PHẠM THẾ TOÀN <thetoan.0112@gmail.com>
2024-12-14 11:00:33 +0700

    update

--- BE/app/Http/Controllers/Post/PostController.php
+++ BE/app/Http/Controllers/Post/PostController.php
@@ -13,7 +13,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::with('categories', 'user')->get();  
+        $posts = Post::with('categories', 'user')->orderBy('id','desc')->get();  
         return view('post.index', compact('posts'));
     }
 
