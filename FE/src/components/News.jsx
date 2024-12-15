e46f25d995b882380b0be744515b3e97cb15ba7e
VoHieu906 <vohieutd09@gmail.com>
2024-12-15 16:01:18 +0700

    add post to homepage

--- FE/src/components/News.jsx
+++ FE/src/components/News.jsx
@@ -1,87 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import camera from "../assets/images/iHome/camera.png";
 import clock from "../assets/images/iHome/hwclock.png";
 import ssphone from "../assets/images/iHome/samsungphone.png";
 import hwphone from "../assets/images/iHome/hwphone.png";
+import { postApi } from "../apis/post";
+import { Link, useNavigate } from "react-router-dom";
 const News = () => {
+  const navigate = useNavigate();
+  const [post, setPost] = useState([]);
+  const handleViewAll = () => {
+    navigate("/post");
+  };
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await postApi.getAll();
+      console.log(res);
+      setPost(res);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="d-flex justify-content-center mt-3">
       <div className="container news-section">
         <div className="d-flex justify-content-between align-items-center mb-3">
           <div className="news-title">TIN CÔNG NGHỆ</div>
           <div className="view-all">
-            <a href="#">Xem tất cả</a>
+            <span style={{ cursor: "pointer" }} onClick={handleViewAll}>
+              Xem tất cả
+            </span>
           </div>
         </div>
         <div className="d-flex justify-content-between">
-          <div className="card">
-            <img
-              alt="DJI Osmo Action 5 Pro camera"
-              className="card-img-top"
-              height={150}
-              src={camera}
-              width={230}
-            />
-            <div className="card-body">
-              <a href="#" className="card-title">
-                <p className="card-title">
-                  DJI ra mắt camera hành động Osmo Action 5 Pro, cạnh tranh trực
-                  tiếp...
-                </p>
-              </a>
-            </div>
-          </div>
-          <div className="card">
-            <img
-              alt="Huawei Watch GT 5 Series and Watch D2"
-              className="card-img-top"
-              height={150}
-              src={clock}
-              width={230}
-            />
-            <div className="card-body">
-              <a href="#" className="card-title">
-                <p className="card-title">
-                  Huawei Watch GT 5 Series và Watch D2 ra mắt với hàng loạt tính
-                  năng...
-                </p>
-              </a>
-            </div>
-          </div>
-          <div className="card">
-            <img
-              alt="Huawei Mate XT Ultimate"
-              className="card-img-top"
-              height={150}
-              src={hwphone}
-              width={230}
-            />
-            <div className="card-body">
-              <a href="#" className="card-title">
-                <p className="card-title">
-                  Huawei Mate XT Ultimate có giá ở “chợ đen” lên tới hơn 245
-                  triệu đồng...
-                </p>
-              </a>
-            </div>
-          </div>
-          <div className="card">
-            <img
-              alt="Samsung Galaxy S25"
-              className="card-img-top"
-              height={150}
-              src={ssphone}
-              width={230}
-            />
-            <div className="card-body">
-              <a href="#" className="card-title">
-                <p className="card-title">
-                  Rò rỉ dung lượng pin Galaxy S25: Không có cải tiến nào đáng
-                  chú ý
-                </p>
-              </a>
-            </div>
-          </div>
+          {post &&
+            post
+              .filter((_, index) => index < 4)
+              .map((item) => (
+                <div key={item.id} className="card">
+                  <Link title={item?.title} to={`/post-detail/${item?.slug}`}>
+                    <img
+                      alt="DJI Osmo Action 5 Pro camera"
+                      className="card-img-top"
+                      height={150}
+                      src={item.images}
+                      width={230}
+                    />
+                  </Link>
+
+                  <div className="card-body">
+                    <a href="#" className="card-title">
+                      <Link
+                        title={item?.title}
+                        to={`/post-detail/${item?.slug}`}
+                      >
+                        {item?.title}
+                      </Link>
+                    </a>
+                  </div>
+                </div>
+              ))}
         </div>
       </div>
     </div>
