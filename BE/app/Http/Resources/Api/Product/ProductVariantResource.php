6ecf1d667e130f0a31212de39fbee22e5d10a270
PHẠM THẾ TOÀN <thetoan.0112@gmail.com>
2024-12-16 08:50:43 +0700

    Update ProductVariantResource.php

--- BE/app/Http/Resources/Api/Product/ProductVariantResource.php
+++ BE/app/Http/Resources/Api/Product/ProductVariantResource.php
@@ -12,37 +12,38 @@ class ProductVariantResource extends JsonResource
      * @return array
      */
     public function toArray($request): array
-{
-    return [                    
-        'category' => [
-            'name' => optional($this->product->category)->name ?? 'Chưa có thông tin',
-        ],
-        'brand' => [
-            'name' => optional($this->product->brand)->name ?? 'Chưa có thông tin',
-        ],
-        'product_variant' => [
-            'id' => $this->id,
-            'sold' => $this->sold,
-            'instock' => $this->instock,
-            'name' => $this->product->name,
+    {
+        return [
+            'id' => optional($this->product)->id,
             'images' => $this->product->images,
             'slug' => $this->product->slug,
-            'sku' => $this->sku,
-            'storage' => $this->storage,
-            'sale' => $this->sale,
-            'price' => $this->price,
-            'images' => $this->images,
-            'color' => $this->color,
-            'is_flash_sale' => $this->is_flash_sale,
-            'flashsale_price' => $this->flashSale->discount_price ?? null,
-        ],
-        'product_image_items' => $this->product->product_image_items->map(function ($item) {
-            return [
-                'id' => $item->id,
-                'name' => $item->name ?? 'Chưa có thông tin',
-                'images' => $item->images ?? 'Chưa có thông tin',
-            ];
-        })->values(),
-    ];
-}
+            'category' => [
+                'name' => optional($this->product->category)->name ?? 'Chưa có thông tin',
+            ],
+            'brand' => [
+                'name' => optional($this->product->brand)->name ?? 'Chưa có thông tin',
+            ],
+            'product_variant' =>
+                [
+                'id' => $this->id,
+                'sku' => $this->sku,
+                'storage' => $this->storage,
+                'sale' => $this->sale,
+                'price' => $this->price,
+                'images' => $this->images,
+                'color' => $this->color,
+                'instock' => $this->instock,
+                'is_flash_sale' => $this->is_flash_sale,
+                'sold' => $this->sold,
+                
+            ],
+            'product_image_items' => $this->product->product_image_items->map(function ($item) {
+                return [
+                    'id' => $item->id,
+                    'name' => $item->name ?? 'Chưa có thông tin',
+                    'images' => $item->images ?? 'Chưa có thông tin',
+                ];
+            })->values(),
+        ];
+    }
 }
