f9de480b91c2aedd5429ea834c0e02f943f3f080
VoHieu906 <vohieutd09@gmail.com>
2024-12-16 19:39:24 +0700

    add header of api

--- FE/src/ultis/func.js
+++ FE/src/ultis/func.js
@@ -107,6 +107,7 @@ export const transformFormatProducts = (product) => {
     end_time: product.end_time,
     sold: product.sold,
     quantity_limit: product.quantity_limit,
+    // total_quanitiy_limit: product.total_quanitiy_limit,
     product_variant: [
       {
         storage: product.product_variant[0]?.storage,
