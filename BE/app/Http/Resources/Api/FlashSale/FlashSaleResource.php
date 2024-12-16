2cbd25b21e29f37de353589ea3a30e8652de5173
PHẠM THẾ TOÀN <thetoan.0112@gmail.com>
2024-12-16 09:31:18 +0700

    Update FlashSaleResource.php

--- BE/app/Http/Resources/Api/FlashSale/FlashSaleResource.php
+++ BE/app/Http/Resources/Api/FlashSale/FlashSaleResource.php
@@ -17,7 +17,7 @@ class FlashSaleResource extends JsonResource
     return [
         'id' => $this->product_variant->product->id,
         'quantity_limit' => $this->quantity_limit,
-        'soldFlashsale' => $this->sold,
+        'sold' => $this->sold,
         'discount_price' => $this->discount_price,
         'is_active' => $this->is_active,
         'name' => $this->product_variant->product->name,
